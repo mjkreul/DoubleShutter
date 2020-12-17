a8d35d370c0e1ffd8ae1bf8e96868ce636288735
Matt Kreul <mjkreul@iastate.edu>
2020-12-17 11:32:30 -0600

    Very initial main creation for game

--- main.cpp
+++ main.cpp
@@ -5,10 +5,12 @@
 
 using namespace std;
 
+void toUpper(string s);
+
 int main() {
 	DSBoard * b = new DSBoard();
 	bool gameContinue = true;
-	char ans;
+	string ans;
 	int nWins, nLosses;
 	while(gameContinue){
 		while(b->gameFinished()){
@@ -17,9 +19,10 @@ int main() {
 			cout << "What would you like to do?" << endl;
 			
 		}
+		if(b.)
 		cout << "Thanks for playing!  Would you like to play again? Y/N: ";
 		cin >> ans;
-		if('Y' || 'y'){
+		if(ans == "YES" || ans == "Y"){
 			b = new DSBoard();
 		}
 		else{
@@ -33,3 +36,4 @@ int main() {
 	
 	return 0;
 }
+
