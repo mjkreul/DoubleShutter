5be337762c50362255205ef4b50cd0d1c32e77bc
Matt Kreul <mjkreul@iastate.edu>
2020-12-17 11:32:50 -0600

    Rudimentary game completed

--- DSBoard.cpp
+++ DSBoard.cpp
@@ -6,34 +6,149 @@
 
 using namespace std;
 
-void DSBoard::hitTile(tile choice, int nTiles) {
-	if(checkValidMove(choice, nTiles)){
-		if(choice == front){
-			if(nTiles == 1){
-				front_tile[(die1 + die2) - 1] = 'X';
-			}
-			else{
-				front_tile[die1 - 1] = 'X';
-				front_tile[die2 - 1] = 'X';
-			}
-		}
-		else {
-		
-		}
+/**
+ *
+ */
+DSBoard::DSBoard() {
+	for(int i = 0; i < 9; i++){
+		front_tile[i] = (i+1);
+		back_tile.push_back(i+1);
 	}
-	
 }
 
+/**
+ *
+ * @param choice
+ * @param t
+ */
+bool DSBoard::hitTile(vector<tile> choice, vector<int> t) {
+	int sum =0;
+	for(int i : t){
+		sum += i;
+	}
+	if(sum != (die1 + die2)){
+		return false;
+	}
+	else{
+		for(int i = 0; i < t.size(); i++){
+			tile tempChoice = choice[i];
+			int tempNum = t[i];
+			if(tempChoice == back){
+				if(front_tile[(9 - tempNum)] == 0){
+					back_tile[tempNum - 1] = 0;
+				}
+				else{
+					return false;
+				}
+			}
+			else{
+				if(front_tile[tempNum - 1] != 0){
+					front_tile[tempNum - 1] = 0;
+				}
+				else{
+					return false;
+				}
+			}
+		}
+	}
+	return true;
+}
+
+/**
+ *
+ */
 void DSBoard::roll() {
 	die1 = rand() % 6 + 1;
 	die2 = rand() % 6 + 1;
 }
 
+/**
+ *
+ * @param choice
+ * @param t
+ * @return
+ */
+//bool DSBoard::checkValidMove(tile choice, int * t) {
+//	return false;
+//}
 
-bool DSBoard::checkValidMove(tile choice, int nTiles) {
-
-}
-
+/**
+ *
+ * @return
+ */
 bool DSBoard::gameFinished() {
 	return this->gameStatus;
 }
+
+/**
+ *
+ */
+void DSBoard::printDie() {
+	string line11, line21, line31, line12, line22, line32;
+	if(die1%2 == 1){
+		line21 = "|   o   |";
+	}
+	if(die1 == 1){
+		line11 = "|       |";
+		line31 = "|       |";
+	}
+	else if (die1 > 1 && die1 < 4){
+		line11 = "| o     |";
+		line31 = "|     o |";
+		if(die1 == 2){
+			line21 = "|       |";
+		}
+	}
+	else if(die1 > 3){
+		line11 = "| o   o |";
+		line31 = "| o   o |";
+		if(die1%2 == 0){
+			if(die1 == 4){
+				line21 = "|       |";
+			}
+			else{
+				line21 = "| o   o |";
+			}
+		}
+	}
+	if(die2%2 == 1){
+		line22 = "|   o   |";
+	}
+	if(die2 == 1){
+		line12 = "|       |";
+		line32 = "|       |";
+	}
+	else if (die2 > 1 && die2 < 4){
+		line12 = "| o     |";
+		line32 = "|     o |";
+		if(die2 == 2){
+			line22 = "|       |";
+		}
+	}
+	else if(die2 > 3){
+		line12 = "| o   o |";
+		line32 = "| o   o |";
+		if(die2%2 == 0){
+			if(die2 == 4){
+				line22 = "|       |";
+			}
+			else{
+				line22 = "| o   o |";
+			}
+		}
+	}
+	cout << line11 << " " << line12 << endl;
+	cout << line21 << " " << line22 << endl;
+	cout << line31 << " " << line32 << endl;
+}
+
+DSBoard::~DSBoard() {
+//	delete front_tile;
+
+}
+
+bool DSBoard::gameWon() {
+	return winStatus;
+}
+
+
--- DSBoard.h
+++ DSBoard.h
@@ -7,31 +7,38 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+#include <vector>
 
 using namespace std;
-
 
 typedef enum tile_e {front = 0, back = 1} tile;
 
 class DSBoard {
 	private:
-		char front_tile[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
-		char back_tile[9] = {'9', '8', '7', '6', '5', '4', '3', '2', '1'};
+//		char front_tile[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+//		char back_tile[9] = {'9', '8', '7', '6', '5', '4', '3', '2', '1'};
+		vector<int> front_tile;
+		vector<int> back_tile;
 		bool gameStatus = true;
+		bool winStatus = false;
 		int die1 = -1;
 		int die2 = -1;
 		//game won or not
 		
 		
 	public:
-//		DSBoard() {}
-		
-		void hitTile(tile choice, int nTiles);
+		DSBoard();
+		~DSBoard();
+		bool hitTile(vector<tile> choice, vector<int> t);
 		void roll();
+		
+//		bool checkValidMove(tile choice, int * t);
+		bool gameFinished();
+		bool gameWon();
 		void printDie();
 		void printBoard();
-		bool checkValidMove(tile choice, int nTiles);
-		bool gameFinished();
+		
+		
 		void printDieTS();
 		void printTiles();
 		
