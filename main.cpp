e17a2f8520352e3d31cff6e65702b4a82292976a
Matt Kreul <mjkreul@iastate.edu>
2020-12-18 12:32:00 -0600

    fixed reading in lines from the user

--- main.cpp
+++ main.cpp
@@ -1,25 +1,45 @@
 /// Author: Matt Kreul
 
 #include <iostream>
+#include <sstream>
 #include "DSBoard.h"
+#include "parseHelper.h"
 
 using namespace std;
 
 void toUpper(string s);
 
 int main() {
+	cout << "Made it past main" << endl;
 	DSBoard * b = new DSBoard();
+	cout << "Made a board" << endl;
 	bool gameContinue = true;
-	string ans;
+	
+	string ans, inputString;
+	
 	int nWins, nLosses;
 	while(gameContinue){
 		while(b->gameFinished()){
 			b->roll();
+			b->printDieTS();
 			b->printBoard();
-			cout << "What would you like to do?" << endl;
+			vector<tile> tempTiles;
+			vector<int> tempNums;
+			vector<string> tokens;
 			
+			cout << "What would you like to do?" << endl;
+			getline(cin, inputString);
+			stringstream ss(inputString);
+			int i = 0;
+			while(getline(ss, ans, ' ')){
+				if(i%2 ==0) tempTiles.push_back(parseHelper::parseTileItem(ans));
+				else tempNums.push_back(parseHelper::parseNumItem(ans));
+				i++;
+			}
+			if(!b->hitTile(tempTiles, tempNums)){
+				cout << "invalid move please try again" << endl;
+			}
 		}
-		if(b.)
 		cout << "Thanks for playing!  Would you like to play again? Y/N: ";
 		cin >> ans;
 		if(ans == "YES" || ans == "Y"){
